39e95c41dcc13b9cde26f1ca1c68437007c1fe98
Chang Jian Rong <cjr030918d@gmail.com>
2025-06-17 20:34:32 +0800

    Add routing for home page

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run()
