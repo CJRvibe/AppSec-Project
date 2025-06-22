e59de80535ee94d3fa5c28cf38f2199d1596026d
lucas <lucasongpc@gmail.com>
2025-06-22 17:32:51 +0800

    Activity Hub html and test image

--- __init__.py
+++ __init__.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
+@app.route('/activity-hub')
+def activity_hub():
+    activities = [
+        {"category": "Music & Vocal", "name": "Karaoke", "image": "/static/img/karaoke.jpg"}
+    ]
+    return render_template('/activity_hub.html', activities=activities)
+
 
 if __name__ == "__main__":
     app.run()
