74753a9462836e1f236ba358b03b0e4914c3248d
Chang Jian Rong <cjr030918d@gmail.com>
2025-06-23 13:09:51 +0800

    Add max_size and join_type form fields to InterestGroupProposalForm

--- forms.py
+++ forms.py
@@ -8,10 +8,17 @@ ACTIVITY_OCCURENCE = [
     ("W", "Weekly")
 ]
 
+JOIN_TYPE = [
+    ("PU", "Public"),
+    ("PR", "Private")
+]
+
 class InterestGroupProposalForm(Form):
     name = StringField("Group Name", [validators.length(min=1, max=100), validators.DataRequired()])
     topic = StringField("Group Topic", [validators.length(min=1, max=100), validators.DataRequired()])
-    description = StringField("Group Description", [validators.length(min=1, max=100)])
+    description = StringField("Group Description", [validators.length(min=1, max=100), validators.DataRequired()])
     activity_occurence = SelectField("Activity Occurrence", [validators.DataRequired()], choices=ACTIVITY_OCCURENCE, default="")
+    max_size = IntegerField("Maximum Group Size", [validators.NumberRange(min=10, max=50), validators.DataRequired()])
+    join_type = RadioField("Group Join Type", [validators.DataRequired()], choices=JOIN_TYPE)
     reason = TextAreaField("Proposal Elaboration", [validators.length(min=1, max=1000), validators.DataRequired()])
-    tags = TextAreaField("Group Tags", [validators.length(min=1, max=300)])
+    tags = TextAreaField("Group Tags", [validators.length(min=1, max=300), validators.Optional()])
