08de60c5d4dd979ffef6ea98b0ffc71f7d551b5f
Chang Jian Rong <cjr030918d@gmail.com>
2025-06-29 21:37:30 +0800

    Add create activity proposal form

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, IntegerField, RadioField, SelectField, TextAreaField, validators
+from wtforms import Form, StringField, IntegerField, RadioField, SelectField, DateField, TextAreaField, validators
 
 ACTIVITY_OCCURENCE = [
     ("", "Select"),
@@ -13,6 +13,17 @@ JOIN_TYPE = [
     ("PR", "Private")
 ]
 
+CC_LOCATION = [
+    ("", "Select"),
+    ("O", "Online"),
+    ("AL", "Aljunied"),
+    ("AN", "Anchorvale"),
+    ("AMK", "Ang Mo Kio"),
+    ("BE", "Bedok"),
+    ("BI", "Bishan"),
+    ("BO", "Boon Lay")
+]
+
 class InterestGroupProposalForm(Form):
     name = StringField("Group Name", [validators.length(min=1, max=100), validators.DataRequired()])
     topic = StringField("Group Topic", [validators.length(min=1, max=100), validators.DataRequired()])
@@ -22,3 +33,15 @@ class InterestGroupProposalForm(Form):
     join_type = RadioField("Group Join Type", [validators.DataRequired()], choices=JOIN_TYPE)
     reason = TextAreaField("Proposal Elaboration", [validators.length(min=1, max=1000), validators.DataRequired()])
     tags = TextAreaField("Group Tags", [validators.length(min=1, max=300), validators.Optional()])
+
+
+class ActivityProposalForm(Form):
+    name = StringField("Activity Name", [validators.DataRequired(), validators.length(min=1,max=30)])
+    category = StringField("Category", [validators.DataRequired(), validators.length(min=1,max=30)])
+    description = StringField("Activity Description", [validators.DataRequired(), validators.length(min=1, max=100)])
+    date = DateField("Activity Date", [validators.DataRequired()])
+    max_size = IntegerField("Max Group Size", [validators.DataRequired(), validators.NumberRange(min=1, max=100)])
+    funds = IntegerField("Fund Request", [validators.DataRequired(), validators.NumberRange(min=50, max=1000)])
+    location = SelectField("Location", [validators.DataRequired()], choices=CC_LOCATION, default="")
+    tags = TextAreaField("Group Tags", [validators.length(min=1, max=300), validators.Optional()])
+    remarks = TextAreaField("Additional remarks", [validators.Optional(), validators.length(min=1, max=300)])
