5c363a5c8afec1c6ebc948e80bdb5c26100736bf
NotAlways <lucasongpc@gmail.com>
2025-07-01 16:51:37 +0800

    activity hub ui changes and initial img upload functionality

--- __init__.py
+++ __init__.py
@@ -1,7 +1,17 @@
+import os
 from flask import Flask, render_template, redirect, url_for, request, abort
 from forms import InterestGroupProposalForm
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -9,6 +19,36 @@ def index():
 
 #test data
 activities = [
+    {
+        "id": 1,
+        "category": "Music & Vocal",
+        "name": "Karaoke",
+        "image": "img/karaoke.jpg",
+        "desc": "Sing your heart out in a private karaoke room!",
+        "timings": "Fridays 7PM - 9PM",
+        "min_age": 16,
+        "requirements": "Must love music and have noise tolerance. No prior singing experience required!",
+    },
+    {
+        "id": 1,
+        "category": "Music & Vocal",
+        "name": "Karaoke",
+        "image": "img/karaoke.jpg",
+        "desc": "Sing your heart out in a private karaoke room!",
+        "timings": "Fridays 7PM - 9PM",
+        "min_age": 16,
+        "requirements": "Must love music and have noise tolerance. No prior singing experience required!",
+    },
+    {
+        "id": 1,
+        "category": "Music & Vocal",
+        "name": "Karaoke",
+        "image": "img/karaoke.jpg",
+        "desc": "Sing your heart out in a private karaoke room!",
+        "timings": "Fridays 7PM - 9PM",
+        "min_age": 16,
+        "requirements": "Must love music and have noise tolerance. No prior singing experience required!",
+    },
     {
         "id": 1,
         "category": "Music & Vocal",
