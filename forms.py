00de578d68d77bd13aaac75f6e75b41645b1f3be
Chang Jian Rong <cjr030918d@gmail.com>
2025-07-03 11:02:28 +0800

    Edit form fields for interest group and interest activity proposal

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, IntegerField, RadioField, SelectField, DateField, TextAreaField, validators
+from wtforms import Form, StringField, IntegerField, RadioField, SelectField, DateField, DateTimeField, TextAreaField, validators
 
 ACTIVITY_OCCURENCE = [
     ("", "Select"),
@@ -25,22 +25,21 @@ CC_LOCATION = [
 ]
 
 class InterestGroupProposalForm(Form):
-    name = StringField("Group Name", [validators.length(min=1, max=100), validators.DataRequired()])
-    topic = StringField("Group Topic", [validators.length(min=1, max=100), validators.DataRequired()])
-    description = StringField("Group Description", [validators.length(min=1, max=100), validators.DataRequired()])
-    activity_occurence = SelectField("Activity Occurrence", [validators.DataRequired()], choices=ACTIVITY_OCCURENCE, default="")
-    max_size = IntegerField("Maximum Group Size", [validators.NumberRange(min=10, max=50), validators.DataRequired()])
+    name = StringField("Group Name", [validators.length(min=1, max=50), validators.DataRequired()])
+    topic = StringField("Group Topic", [validators.length(min=1, max=50), validators.DataRequired()])
+    description = StringField("Group Description", [validators.length(min=1, max=200), validators.DataRequired()])
+    max_size = IntegerField("Maximum Group Size", [validators.NumberRange(min=10, max=100), validators.DataRequired()])
     join_type = RadioField("Group Join Type", [validators.DataRequired()], choices=JOIN_TYPE)
+    activity_occurence = SelectField("Activity Occurrence", [validators.DataRequired()], choices=ACTIVITY_OCCURENCE, default="")
     reason = TextAreaField("Proposal Elaboration", [validators.length(min=1, max=1000), validators.DataRequired()])
-    tags = TextAreaField("Group Tags", [validators.length(min=1, max=300), validators.Optional()])
 
 
 class ActivityProposalForm(Form):
-    name = StringField("Activity Name", [validators.DataRequired(), validators.length(min=1,max=30)])
-    category = StringField("Category", [validators.DataRequired(), validators.length(min=1,max=30)])
-    description = StringField("Activity Description", [validators.DataRequired(), validators.length(min=1, max=100)])
-    date = DateField("Activity Date", [validators.DataRequired()])
-    max_size = IntegerField("Max Group Size", [validators.DataRequired(), validators.NumberRange(min=1, max=100)])
+    name = StringField("Activity Name", [validators.DataRequired(), validators.length(min=1,max=50)])
+    description = StringField("Activity Description", [validators.DataRequired(), validators.length(min=1, max=200)])
+    start_datetime = DateTimeField("Activity Start Date", [validators.DataRequired()])
+    end_datetime = DateTimeField("Activity End Date", [validators.DataRequired()])
+    max_size = IntegerField("Max Group Size", [validators.DataRequired(), validators.NumberRange(min=1, max=50)])
     funds = IntegerField("Fund Request", [validators.DataRequired(), validators.NumberRange(min=50, max=1000)])
     location = SelectField("Location", [validators.DataRequired()], choices=CC_LOCATION, default="")
     tags = TextAreaField("Group Tags", [validators.length(min=1, max=300), validators.Optional()])
