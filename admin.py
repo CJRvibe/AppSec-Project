11698bbd2468bc78c5ae61e45662661993b1765e
Chang Jian Rong <cjr030918d@gmail.com>
2025-07-15 14:30:48 +0800

    Modify admin URLs

--- admin.py
+++ admin.py
@@ -4,28 +4,28 @@ import db
 admin = Blueprint("admin", __name__, template_folder="templates")
 
 
-@admin.route("/manageInterestGroupProposals")
+@admin.route("/interestGroups/proposals")
 def manage_group_proposals():
     proposals = db.get_group_proposals()
 
     return render_template("admin/manage_group_proposals.html", proposals=proposals)
 
 
-@admin.route("/manageActiveGroups")
+@admin.route("/interestGroups")
 def manage_active_groups():
     groups = db.get_active_groups()
 
     return render_template("admin/manage_active_groups.html", groups=groups)
 
 
-@admin.route("/manageRejectGroups")
+@admin.route("/interestGroups/rejectedGroups")
 def manage_reject_groups():
     groups = db.get_reject_groups()
-    
+
     return render_template("admin/manage_reject_groups.html", groups=groups)
 
 
-@admin.route("/viewGroupProposal/<int:id>")
+@admin.route("/interestGroups/proposals/<int:id>")
 def view_group_proposal(id):
     proposal = db.get_group_proposal(id)
     print(proposal)
@@ -36,14 +36,14 @@ def view_group_proposal(id):
     return redirect(url_for(".manage_group_proposals"))
 
 
-@admin.route("/approveGroupProposal/<int:id>", methods=["POST"])
+@admin.route("/interestGroups/approveGroupProposal/<int:id>", methods=["POST"])
 def approve_group_proposal(id):
     db.update_group_proposal(id, approved=True)
     
     return redirect(url_for(".manage_group_proposals"))
 
 
-@admin.route("/rejectGroupProposal/<int:id>", methods=["POST"])
+@admin.route("/interestGroups/rejectGroupProposal/<int:id>", methods=["POST"])
 def reject_group_proposal(id):
     db.update_group_proposal(id, approved=False)
     
